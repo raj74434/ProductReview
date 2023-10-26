ec62d92ef71e4cce57cf04bbd51d3795b160caf6
raj74434 <raj74434@gmail.com>
2023-10-26 22:08:25 +0530

    spring boot base application created

---
+++ productReview/src/main/java/com/productReview/productReview/ProductReviewApplication.java
@@ -0,0 +1,13 @@
+package com.productReview.productReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductReviewApplication.class, args);
+	}
+
+}
